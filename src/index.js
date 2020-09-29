ef4c8072307b2cc907b1f1a8bffce2855458e821
ehddnr301 <dy950328@gmail.com>
2020-09-29 19:23:28 +0900

    test

--- src/index.js
+++ src/index.js
@@ -8,17 +8,16 @@ mongoose.connect("mongodb://localhost:27017/cap", {
   useFindAndModify: false,
 });
 
-const db = mongoose.connection;
+// const db = mongoose.connection;
 
-const handleOpen = () => console.log("✅  Connected to DB");
-const handleError = (error) =>
-  console.log(`❌ Error on DB Connection:${error}`);
+// const handleOpen = () => console.log("✅  Connected to DB");
+// const handleError = (error) =>
+//   console.log(`❌ Error on DB Connection:${error}`);
 
-db.once("open", handleOpen);
-db.on("error", handleError);
+// db.once("open", handleOpen);
+// db.on("error", handleError);
 
-const handleListening = () =>
-  console.log(`✅  Listening on: http://localhost:4000`);
+const handleListening = () => console.log(`✅ Hi`);
 
 app.listen(4000, handleListening);
 
